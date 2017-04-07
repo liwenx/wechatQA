63c77269c18d9e7846b609e6f1fb90d95b6e975c
13281060 <13281060@bjtu.edu.cn>
2017-04-07 10:45:58 +0800

    集成百度翻译，完成版
    Signed-off-by: 13281060 <13281060@bjtu.edu.cn>

--- src/main/java/com/bjtu/lwx/service/impl/ReplyText.java
+++ src/main/java/com/bjtu/lwx/service/impl/ReplyText.java
@@ -38,9 +38,7 @@ public class ReplyText {
 		//百度翻译服务
 		else if ("翻译".equals(type.substring(0, 2))){
 			String from = "auto";
-			System.out.println(btDao.getAbbByName(type.substring(3,5)));
-//			String to = this.btDao.getAbbByName(type.substring(3,5)).get(0).getLanguageAbb();
-			String to = "en";
+			String to = this.btDao.getAbbByName(type.substring(3,5)).get(0).getLanguageAbb();
 			System.out.println(to);
 			
 			 TransApi api = new TransApi(BaiduTranslateConstant.APP_ID,BaiduTranslateConstant.SECURITY_KEY);
@@ -50,11 +48,8 @@ public class ReplyText {
 			 JSONObject jsonObject=JSONObject.fromObject(translateJson);
 			 List<Map<String, String>> trans_result = new ArrayList<Map<String,String>>();
 			 trans_result = jsonObject.getJSONArray("trans_result");
-			 Map<String,String> result = new HashMap();
-			 
-			 result = trans_result.get(0);
-			 
-			 
+			 Map<String,String> result = new HashMap();			 
+			 result = trans_result.get(0);			 			 
 			 StringBuffer sb = new StringBuffer();
 			 sb.append(result.get("dst"));
 			 
