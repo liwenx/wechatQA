307036bda6b880db799a5191f5431a24bc9e12c7
13281060 <13281060@bjtu.edu.cn>
2017-04-10 16:25:40 +0800

    OCR处理，已实现，待完善
    Signed-off-by: 13281060 <13281060@bjtu.edu.cn>

--- src/main/java/com/bjtu/lwx/service/impl/ReplyImage.java
+++ src/main/java/com/bjtu/lwx/service/impl/ReplyImage.java
@@ -7,8 +7,13 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Component;
 
 import com.bjtu.lwx.util.GetAccessToken;
+import com.bjtu.lwx.util.MessageUtil;
 import com.bjtu.lwx.util.WeixinFetchTempFile;
 import com.bjtu.lwx.vo.ImageMessageVO;
+import com.bjtu.lwx.vo.TextMessageVO;
+
+import net.sourceforge.tess4j.Tesseract;
+import net.sourceforge.tess4j.TesseractException;
 
 /**
  * 回复图片消息 
@@ -23,16 +28,34 @@ public class ReplyImage {
 	@Resource
 	private WeixinFetchTempFile wxftfile;
 	
+	@Resource
+	private TextMessageVO msgvo;
+	
 	public String reply (ImageMessageVO imvo) {
 		
+		String message = null;
 		String token = gatoken.getAccessToken();
-//		System.out.println(token);
-//		File file = new WeixinFetchTempFile().fetchTmpFile(imvo.getMediaId(), imvo.getMsgType(),token);
-		 String filename = wxftfile.fetchTmpFile(imvo.getMediaId(), imvo.getMsgType(),token);
+		String filename = wxftfile.fetchTmpFile(imvo.getMediaId(), imvo.getMsgType(),token);
 		 
 		 //获取到用户发的图片，然后进行OCR处理
+		String path = "e:\\wechatimage\\";
+		File imageFile=new File(path+filename);
+//		File imageFile=new File(path+"1.png");
+
+		Tesseract instance=new Tesseract();
+		instance.setDatapath("E:\\wechatimage\\tessdata");//设置训练库的位置
+		instance.setLanguage("chi_sim");//中文识别
+		String result = null;
+		try {
+			 result=instance.doOCR(imageFile);
+			System.out.println(result);
+		} catch (TesseractException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		return null;
+		message = MessageUtil.initText(imvo.getToUserName(), imvo.getFromUserName(),result);
+		return message;
 		
 	}
 
