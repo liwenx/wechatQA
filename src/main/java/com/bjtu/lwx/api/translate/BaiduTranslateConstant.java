da48a3594517ed577cc29c975e3da74d78388db3
13281060 <13281060@bjtu.edu.cn>
2017-08-28 21:00:04 +0800

    哈哈
    Signed-off-by: 13281060 <13281060@bjtu.edu.cn>

--- src/main/java/com/bjtu/lwx/api/translate/BaiduTranslateConstant.java
+++ src/main/java/com/bjtu/lwx/api/translate/BaiduTranslateConstant.java
@@ -6,12 +6,12 @@ public class BaiduTranslateConstant {
     public static final String APP_ID = "20170406000044270";
     public static final String SECURITY_KEY = "svX9TbBS_QumGDJAWOb3";
     
-//    public static void main(String[] args) throws Exception {
-//        TransApi api = new TransApi(APP_ID, SECURITY_KEY);
-//
-//        String query = "高度600米";
-//        System.out.println(api.getTransResult(query, "auto", "en"));
-//    }
+    public static void main(String[] args) throws Exception {
+        TransApi api = new TransApi(APP_ID, SECURITY_KEY);
+
+        String query = "黎文兴所有";
+        System.out.println(api.getTransResult(query, "auto", "en"));
+    }
     
     
 }
